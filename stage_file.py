7a9fc8595894430d214be2f9412be59484931b47
avi rapa <kenblock.ar@gmail.com>
2026-02-02 15:10:02 +0200

    שלב 6

--- stage_file.py
+++ stage_file.py
@@ -24,10 +24,50 @@ def attempt_transmission(packet):
     except OutOfRangeError:
         raise BrokenConnectionError("Target out of range")
 
+
 def smart_send_packet(packet, ent_list):
+    print("Distance too great Searching for satellite path...")
+    entities = sorted(ent_list, key=lambda satellite: satellite.distance_from_earth)
+
+    route = []
+    visited = set()
+    current = packet.sender
+    target = packet.receiver
+
+    while True:
+        if abs(target.distance_from_earth - current.distance_from_earth) <= 150:
+            route.append(target)
+            break
+
+        options = []
+
+        for e in entities:
+            if e is current or e in visited:
+                continue
+
+            dist = e.distance_from_earth - current.distance_from_earth
+            target_dir = target.distance_from_earth - current.distance_from_earth
+            if abs(dist) <= 150 and dist * target_dir > 0:
+                options.append(e)
+
+        if not options:
+            raise BrokenConnectionError("No valid route to target")
+
+        next_jump = max(options,key=lambda e: abs(e.distance_from_earth - current.distance_from_earth))
+        route.append(next_jump)
+        visited.add(next_jump)
+        current = next_jump
 
 
 
+    print("Route found:", " -> ".join(e.name for e in route))
+
+    prev = packet.sender
+
+    for jump in route:
+        relay = RelayPacket(Packet(packet.data, prev, jump), prev, jump)
+        attempt_transmission(relay)
+        prev = jump
 
 
 class RelayPacket(Packet):
@@ -57,6 +97,3 @@ class Earth(SpaceEntity):
 
     def receive_signal(self, packet: Packet):
         print(f"{self.name} Received: {packet}")
-
-
-
